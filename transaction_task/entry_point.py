d87d63e179adfdfc9a5b4f51ad28aaf6e7e981c3
alinonax <alinonax@gmail.com>
2019-07-30 14:56:35 +0400

    refactor menu

--- transaction_task/entry_point.py
+++ transaction_task/entry_point.py
@@ -5,46 +5,51 @@ from sys import argv
 from file_writer import FileWriter
 from file_reader import FileReader
 
-main_menu = {'1': 'Write to file',
-             '2': 'Read from file',
-             '0': 'Exit program'}
+main_menu = ('''
+1. Write to file
+2. Read from file
+0. Exit program
+''')
 
-write_menu = {'1': 'Set header',
-              '2': 'Add transaction',
-              '3': 'Set transaction',
-              '4': 'Create file',
-              '5': 'Back'}
+write_menu = ('''
+1. Set header
+2. Add transaction
+3. Set transaction
+4. Create file
+5. Back
+''')
 
-header_menu = {'1': 'Set name',
-               '2': 'Set surname',
-               '3': 'Set patronymic',
-               '4': 'Set address',
-               '5': 'Back'}
+header_menu = ('''
+1. Set name
+2. Set surname
+3. Set patronymic
+4. Set address
+5. Back
+''')
 
-trans_menu = {'1': 'Set transaction sum',
-              '2': 'Set currency code',
-              '3': 'Back'}
+trans_menu = ('''
+1. Set transaction sum
+2. Set currency code
+3. Back
+''')
 
-read_menu = {'1': 'Add file path', 
-             '2': 'Choose field',
-             '3': 'Back'}
+read_menu = ('''
+1. Add file path
+2. Choose field
+3. Back
+''')
 
-field_menu = {'1': 'name',
-              '2': 'surname',
-              '3': 'patronymic',
-              '4': 'address',
-              '5': 'transaction sum',
-              '6': 'currency code',
-              '7': 'transaction number',
-              '8': 'transaction amount',
-              '9': 'back'}
-
-
-def print_ordered_menu(our_dict):
-    options = our_dict.keys()
-    options.sort()
-    for entry in options:
-        print entry, our_dict[entry]
+field_menu = ('''
+1. name
+2. surname
+3. patronymic
+4. address
+5. transaction sum
+6. currency code
+7. transaction number
+8. transaction amount
+9. back
+''')
 
 
 def create_parser():
@@ -57,13 +62,13 @@ if __name__ == '__main__':
     write_obj = FileWriter()
     read_obj = FileReader()
 
-    print_ordered_menu(main_menu)
+    print(main_menu)
     choice = raw_input('Please select: ')
     if choice == '1':  # write to file
-        print_ordered_menu(write_menu)
+        print(write_menu)
         choice = raw_input('Please select: ')
         if choice == '1':  # set header
-            print_ordered_menu(header_menu)
+            print(header_menu)
             choice = raw_input('Please select: ')
             if choice == '1':  # set name
                 user_name = raw_input("Enter name: ")
@@ -78,12 +83,13 @@ if __name__ == '__main__':
                 user_address = raw_input("Enter address: ")
                 write_obj.set_header('address', user_address)
             elif choice == '5':  # back
-                print_ordered_menu(write_menu)
+                print(write_menu)
             else:
                 print('Unknown option selected!')
         elif choice == '2':  # add transaction
             pass
         elif choice == '3':  # set transaction
+            print(trans_menu)
             if choice == '1':  # set transaction sum
                 user_trans_sum = raw_input('Enter transaction sum: ')
                 write_obj.set_trans('trans_sum', user_trans_sum)
@@ -91,17 +97,17 @@ if __name__ == '__main__':
                 user_currency_code = raw_input('Enter currency code: ')
                 write_obj.set_trans('trans_sum', user_currency_code)
             elif choice == '3':  # back
-                print_ordered_menu(write_menu)
+                print(write_menu)
             else:
                 print('Unknown option selected!')
         elif choice == '4':  # create file
             write_obj.create_file()
         elif choice == '5':  # back
-            print_ordered_menu(write_menu)
+            print(write_menu)
         else:
             print('Unknown option selected!')
     elif choice == '2':  # read from file
-        print_ordered_menu(read_menu)
+        print(read_menu)
         choice = raw_input('Please select: ')
         if choice == '1':  # add file path
             user_folder = raw_input('Enter folder: ')
@@ -109,7 +115,7 @@ if __name__ == '__main__':
             read_obj.set_file_path(user_folder, user_file_name)
             read_obj.read_file()
         elif choice == '2':  # choose field
-            print_ordered_menu(field_menu)
+            print(field_menu)
             choice = raw_input('Please select: ')
             if choice == '1':
                 print(read_obj.header.name)
@@ -130,9 +136,9 @@ if __name__ == '__main__':
             elif choice == '8':
                 print(read_obj.trailer.trailer_trans_amount)
             elif choice == '9':
-                print_ordered_menu(read_menu)
+                print(read_menu)
         elif choice == '3':  # back
-            print_ordered_menu(main_menu)
+            print(main_menu)
     elif choice == '0':  # exit program
         sys.exit()
     else:
